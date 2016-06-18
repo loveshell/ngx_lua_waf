4eb8d59b833773f86631be2a598e588fe2d913ed
ibuler <ibuler@qq.com>
2016-06-18 15:03:28 +0800

    拆分文件

---
+++ access.lua
@@ -0,0 +1,5 @@
+if cc_deny then
+    denyCC(cc_rate, cc_deny_seconds)
+else
+    return
+end
---
+++ entry.lua
@@ -0,0 +1,10 @@
+--------- Global default config -------
+require 'config'
+--------- Local config setting --------
+cc_deny = false
+cc_rate = '10/60'
+
+--------- Init project ----------------
+require 'init'
+--------- Access control limit --------
+require ''
